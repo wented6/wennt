a66ee3c1af47df9ee35396b5649764ded9412105
wented6 <44713416+wented6@users.noreply.github.com>
2018-11-09 19:16:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "!";
+const adminprefix = "!";
 const devs = ['228139766573432832','228139766573432832'];
 /////////////////////////
 ////////////////////////
