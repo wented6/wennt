3f6e5b7ad84291c9ab2e3ef96ce8aeae42873ad9
wented6 <44713416+wented6@users.noreply.github.com>
2018-11-13 14:33:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -359,4 +359,20 @@ if (message.content.startsWith(adminprefix + 'ستريم')) {
 }
 });
 
+
+
+client.on('ready',async () => {
+console.log("Starting..");
+let g = client.guilds.get("504685916023947264");
+let c = g.channels.get("510806482925781032");
+if(c.type === 'voice') {
+c.join();
+setInterval(() => {
+if(!g.me.voiceChannel) c.join();
+}, 1);
+} else {
+console.log("Failed To Join:\n The Channel Type isn't \"text\"");
+}
+});
+
 client.login(process.env.BOT_TOKEN);
