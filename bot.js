927c34109e45fe4c2609f0f2786ef929a9fabaf5
wented6 <44713416+wented6@users.noreply.github.com>
2018-11-17 18:58:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -436,4 +436,24 @@ client.on('message', message => {
 
 
 
+
+
+
+client.on('message', ra3d => {
+var prefix = "!";
+                        let args = ra3d.content.split(" ").slice(1).join(" ")
+if(ra3d.content.startsWith(prefix + 'الوان')) {
+    if(!args) return ra3d.channel.send('`يرجي اختيار كم لون `');
+             if (!ra3d.member.hasPermission('MANAGE_ROLES')) return ra3d.channel.sendMessage('`**⚠ | `[MANAGE_ROLES]` لا يوجد لديك صلاحية**'); 
+              ra3d.channel.send(`**✅ |Created __${args}__ Colors**`);
+                  setInterval(function(){})
+                    let count = 0;
+                    let ecount = 0;
+          for(let x = 1; x < `${parseInt(args)+1}`; x++){
+            ra3d.guild.createRole({name:x,
+              color: 'RANDOM'})
+              }
+            }
+       });
+
 client.login(process.env.BOT_TOKEN);
