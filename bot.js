198db0d747aa1f1002d23da3a042b1e127108387
wented6 <44713416+wented6@users.noreply.github.com>
2018-11-20 17:38:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -397,25 +397,22 @@ console.log("Failed To Join:\n The Channel Type isn't \"text\"");
 
 
 
-
-
-
-
-client.on('message', ra3d => {
-var prefix = "!";
-                        let args = ra3d.content.split(" ").slice(1).join(" ")
-if(ra3d.content.startsWith(prefix + 'الوان')) {
-    if(!args) return ra3d.channel.send('`يرجي اختيار كم لون `');
-             if (!ra3d.member.hasPermission('MANAGE_ROLES')) return ra3d.channel.sendMessage('`**⚠ | `[MANAGE_ROLES]` لا يوجد لديك صلاحية**'); 
-              ra3d.channel.send(`**✅ |Created __${args}__ Colors**`);
-                  setInterval(function(){})
-                    let count = 0;
-                    let ecount = 0;
-          for(let x = 1; x < `${parseInt(args)+1}`; x++){
-            ra3d.guild.createRole({name:x,
-              color: 'RANDOM'})
-              }
+client.on('message', message => {
+    if (message.content === 'وين') {
+          let count = 1;
+          let ecount = 1;
+          for(let x = 1; x < 99999; x++) {
+            message.channel.send(`ٍٍٍ`)
+              .then(m => {
+                count++;
+              })
+              
             }
-       });
+          }
+    });
+
+
+
+
 
 client.login(process.env.BOT_TOKEN);
