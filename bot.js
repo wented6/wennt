52d6d93066d66e7ba3c72fb08a9cc2761e27573c
wented6 <44713416+wented6@users.noreply.github.com>
2018-11-20 17:49:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -397,19 +397,26 @@ console.log("Failed To Join:\n The Channel Type isn't \"text\"");
 
 
 
-client.on('message', message => {
-    if (message.content === 'وين') {
-          let count = 1;
-          let ecount = 1;
-          for(let x = 1; x < 99999; x++) {
-            message.channel.send(`We.`)
-              .then(m => {
-                count++;
-              })
-              
-            }
+client.on("message", message => {
+              var args = message.content.substring(prefix.length).split(" ");
+              if (message.content.startsWith(prefix + "مسح")) {
+                  if(!message.channel.guild) return message.reply('**:x: اسف لكن هذا الامر للسيرفرات فقط **');         
+     if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**⚠  لا يوجد لديك صلاحية لمسح الشات**');
+          var msg;
+          msg = parseInt();
+        
+        message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+        message.channel.sendMessage("", {embed: {
+          title: "``تــم مسح الشات ``",
+          color: 0x06DF00,
+          footer: {
+            
           }
-    });
+        }}).then(msg => {msg.delete(3000)});
+                            }
+  
+       
+  });
 
 
 
