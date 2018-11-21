78581779066ef3a1cc227c392f7786d1f08f099d
wented6 <44713416+wented6@users.noreply.github.com>
2018-11-21 19:53:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -99,7 +99,7 @@ client.on('message', async msg => {
 	let command = msg.content.toLowerCase().split(" ")[0];
 	command = command.slice(prefix.length)
 
-	if (command === `p,play`) {
+	if (command === ''p,play'') {
 		const voiceChannel = msg.member.voiceChannel;
         
         if (!voiceChannel) return msg.channel.send(":x:  **..   ! انت غير موجود بروم صوتي**");
