25370a4be237cc0099898e8271c3698fefe602c9
wented6 <44713416+wented6@users.noreply.github.com>
2018-11-22 23:12:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -310,16 +310,3 @@ function isYoutube(str) {
 
 
 
-client.on('ready',async () => {
-console.log("Starting..");
-let g = client.guilds.get("504685916023947264");
-let c = g.channels.get("510806482925781032");
-if(c.type === 'voice') {
-c.join();
-setInterval(() => {
-if(!g.me.voiceChannel) c.join();
-}, 1);
-} else {
-console.log("Failed To Join:\n The Channel Type isn't \"text\"");
-}
-});
